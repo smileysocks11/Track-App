50ea6c6aa41572cc4a55500a0214af2b6602f5b6
Landon Marcotte <83979177+Landon-Marcotte@users.noreply.github.com>
2023-03-31 14:23:31 -0500

    throw

---
+++ throw/throw/throw.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>	
+using namespace std;
+
+void throwTurns(string athleteArray[], int flights, int athletes);
+
+void throwTurns(string athleteArray[], int flights, int athletes)
+{
+	/*use the loop to loop thorugh the array until all the athles from a single flight has
+	the array is a 3D array where the first colum has the names of the students
+	the second has the school they go to and the third has their best throw
+	A failed attempt is a DQ in the stystem
+	fill the first flight with more people than tht others if there are left*/
+}
