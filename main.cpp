0ac41c3b64822b037fbca2476810a4d5e60ab97b
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-04-04 14:29:39 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+// function prototypes
+void getAthletes(vector<string>, string*, string*, double*);
+
+int main()
+{
+	// declares variables
+	vector<string> lines;
+	string line;
+
+	// opens the file
+	ifstream in_file("C:\\Users\\SSC00042\\Desktop\\data_files\\athlete-database.txt");
+
+	// loops to read through each line in the file
+	// adds each line to the vector
+	int index = 0;
+	while (getline(in_file, line))
+	{
+		lines.push_back(line);
+		index++;
+	}
+	
+	// creates three pointer arrays for name, school, and max
+	// based on the size of the student vector
+	string* name = nullptr;
+	name = new string[lines.size()];
+	string* school = nullptr;
+	school = new string[lines.size()];
+	double* max = nullptr;
+	max = new double[lines.size()];
+
+	// calls getAthletes to input all of the athletes
+	// into a tri-parallel array
+	getAthletes(lines, name, school, max);
+
+
+	// deletes the pointer arrays
+	delete[] name;
+	name = nullptr;
+	delete[] school;
+	school = nullptr;
+	delete[] max;
+	max = nullptr;
+
+	in_file.close();
+	return 0;
+
+
+	// string* ThrowsAway = nullptr;
+	// ThrowsAway = new string[athletes * num_athletes * flights]
+}
