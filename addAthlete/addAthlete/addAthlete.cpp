aad4e34d896de66baefc50c1ec480426ae706377
NoahMcGinley <91349694+NoahMcGinley@users.noreply.github.com>
2023-04-13 14:46:04 -0500

    Worked on addAthlete

--- addAthlete/addAthlete/addAthlete.cpp
+++ addAthlete/addAthlete/addAthlete.cpp
@@ -1,47 +1,24 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <vector>
 using namespace std;
-
-// Prototype functions
-void updateFile(string[], string[], string[], int);
-void addAthlete();
-void readFile();
-
-// Temp function
-int main()
-{
-	addAthlete();
-}
 
 /*--------------------------------
 # addAthlete accepts no arguments
-# it calls readFile() and scoreSort()
+# It asks the user for the new athlete's
+# name, max, and school. It then appends
+# the information to the athletes file
+# matching the existing format
 ----------------------------------*/
 void addAthlete()
 {
-	const int SIZE = 4;
-	string name[SIZE] = { "a b", "b c", "c d", "d e" };
-	string max[SIZE] = { "10-1", "11-2", "12-3", "13-4" };
-	string school[SIZE] = { "w", "x", "y", "z" };
-	updateFile(name, max, school, SIZE);
-}
-
-/*--------------------------------
-# addAthlete accepts a string array
-# It asks the name of the new athlete
-# and adds it to the vector. It then
-# writes the updated vector to the file
-----------------------------------*/
-void updateFile(string name[], string max[], string school[], int SIZE)
-{
 	// Variables and files
-	ofstream athleteFile(R"(C:\Users\SNM00013\Desktop\CPP-2022\files\data_files\AthletesFile.txt)"); // Placeholder file
+	ofstream athleteFile(R"(C:\Users\SNM00013\Desktop\CPP-2022\files\data_files\AthletesFile.txt)", ios_base::app); // Placeholder file
 	string newFirstname;
 	string newLastname;
 	string newMax;
 	string newSchool;
+	char confirm = 'n';
 
 	// Validates file
 	if (!athleteFile)
@@ -50,70 +27,44 @@ void updateFile(string name[], string max[], string school[], int SIZE)
 	}
 	else
 	{
-		// Takes input for first name
-		cout << "Enter the first name of the athlete you would like to enter: ";
-		getline(cin, newFirstname);
-
-		// Takes input for last name
-		cout << "Enter their last name: ";
-		getline(cin, newLastname);
-
-		// Takes input for max
-		cout << "Enter their max (ie. 100-0): ";
-		getline(cin, newMax);
-		
-		// Takes input for name of school
-		cout << "Enter the name of their school: ";
-		getline(cin, newSchool);
-
-		// Loops through the tri-parallel arrays to rewrite them to the file
-		for (int i = 0; i < SIZE; i++)
+		while (tolower(confirm) == 'n')
 		{
-			athleteFile << name[i] << "#" << max[i] << "#" << school[i] << endl;
+			// Takes input for first name
+			cout << "\nEnter the first name of the athlete you would like to add: ";
+			getline(cin, newFirstname);
+
+			// Takes input for last name
+			cout << "Enter their last name: ";
+			getline(cin, newLastname);
+
+			// Takes input for max
+			cout << "Enter their max (ie. 100-0.0). Enter -1 if they do not have a max: ";
+			getline(cin, newMax);
+
+			// Takes input for name of school
+			cout << "Enter the name of their school: ";
+			getline(cin, newSchool);
+
+			// Outputs what user has entered and asks them if it is correct
+			cout << "\nYou entered:\n"
+				<< newFirstname << endl
+				<< newLastname << endl
+				<< newMax << endl
+				<< newSchool << endl
+				<< "Does this information look correct? (y/n): ";
+			cin >> confirm;
+
+			// Clears keyboard buffer if a repeat is neccessary
+			if (tolower(confirm) == 'n')
+			{
+				cin.ignore();
+			}
 		}
 
 		// Writes the new athlete's information to the file
 		athleteFile << newLastname << ", " << newFirstname << "#" << newMax << "#" << newSchool << endl;
 
-		//readFile();
-
+		// Closes file
+		athleteFile.close();
 	}
-	athleteFile.close();
-}
-
-/*--------------------------------
-# readFile accepts no arguments
-# It opens the file of athletes
-# and puts the names into a vector
-----------------------------------*/
-void readFile()
-{
-	// Variables and files
-	ifstream athleteFile(R"(C:\Users\SNM00013\Desktop\CPP-2022\files\data_files\AthletesFile.txt)"); // Placeholder file
-	vector<string> nameList;
-	string name;
-	string newAthlete;
-	int i = 0;
-
-	// Validates file
-	if (!athleteFile)
-	{
-		cout << "File not found.\n";
-	}
-	else
-	{
-		// Reads file to add to vector
-		while (getline(athleteFile, name))
-		{
-			nameList.push_back(name);
-			i++;
-		}
-
-		for (int i = 0; i < nameList.size(); i++)
-		{
-			cout << nameList[0] << endl;
-		}
-	}
-	athleteFile.close();
-
 }
