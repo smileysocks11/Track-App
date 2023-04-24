938d28149304408bf9fce81917afdfb97f23bfa0
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-04-24 14:31:21 -0500

    Update scoreSort.cpp

--- scoreSort.cpp
+++ scoreSort.cpp
@@ -29,7 +29,7 @@ void swapAthletes(Athletes& a, Athletes& b) {
 void sortAthletes(Athletes athletes[], int numAthletes) {
     for (int i = 0; i < numAthletes - 1; i++) {
         for (int j = 0; j < numAthletes - i - 1; j++) {
-            if (athletes[j].feet > athletes[j + 1].feet)
+            if (athletes[j].feet > athletes[j + 1].feet && athletes[j].score != "-1")
                 swapAthletes(athletes[j], athletes[j + 1]);
             else if (athletes[j].inches > athletes[j + 1].inches)
                 swapAthletes(athletes[j], athletes[j + 1]);
@@ -37,35 +37,43 @@ void sortAthletes(Athletes athletes[], int numAthletes) {
     }
 }
 
-Athletes* scoreSort(Athletes* athletes) {
+void scoreSort(Athletes* athletes) {
 
-    ifstream inputFile("C:\\Users\\SSC00042\\Desktop\\raw-athlete-database.txt"); // Open the input file
+    ifstream inputFile("C:\\track_data\\raw-athletes.txt"); // Open the input file
     if (!inputFile) { // Check if the input file was opened successfully
         cerr << "Error opening file" << endl; // Print an error message if the file could not be opened
     }
 
     int numAthletes = 0; // Counter to keep track of the number of Athlete objects read from the file
 
-    while (inputFile && numAthletes < MAX_ATHLETES) { // Read Athlete objects from the file until the end of file is reached
+    string line;
+    while (getline(inputFile, line)) { // Read Athlete objects from the file until the end of file is reached
 
-        string line;
-        getline(inputFile, line);
-        cout << line;
+        try {
+            size_t pos1 = line.find("#"); // Find the position of the first hash symbol
+            size_t pos2 = line.find_last_of("#"); // Find the position of the last hash symbol
+            size_t pos3 = line.find_last_of('-');
+            athletes[numAthletes].name = line.substr(0, pos1); // Extract the name from the line
+            athletes[numAthletes].score = line.substr(pos1 + 1, pos2 - pos1 - 1); // Extract the score from the line
+            athletes[numAthletes].school = line.substr(pos2 + 1); // Extract the school from the line
+            if (athletes[numAthletes].score != "-1")
+            {
+                athletes[numAthletes].feet = stod(line.substr(pos1 + 1, pos3 - pos1 - 1));
+                athletes[numAthletes].inches = stod(line.substr(pos3 + 1, pos2 - pos1 - 1));
+            }
+            numAthletes++;
+            cout << line << endl;
+        } 
+        catch (exception)
+        {
+            cout << numAthletes << endl;
 
-        size_t pos1 = line.find("#"); // Find the position of the first hash symbol
-        size_t pos2 = line.find_last_of("#"); // Find the position of the last hash symbol
-        size_t pos3 = line.find_last_of('-');
-        athletes[numAthletes].name = line.substr(0, pos1); // Extract the name from the line
-        athletes[numAthletes].score = line.substr(pos1 + 1, pos2 - pos1 - 1); // Extract the score from the line and convert it to a double
-        athletes[numAthletes].feet = stod(line.substr(pos1 + 1, pos3 - 1));
-        athletes[numAthletes].inches = stod(line.substr(pos3 + 1, pos2 - 1));
-        athletes[numAthletes].school = line.substr(pos2 + 1); // Extract the school from the line
-        numAthletes++; // Increment the counter for the number of Athlete objects read
+        }
     }
 
     sortAthletes(athletes, numAthletes); // Sort the array of Athletes in ascending order of score using bubble sort
     inputFile.close();
-    ofstream outputFile("C:\\Users\\SSC00042\\Desktop\\sorted-athlete-database.txt"); // Open the output file
+    ofstream outputFile("C:\\track_data\\sorted-athletes.txt"); // Open the output file
     if (!outputFile) { // Check if the output file was opened successfully
         cerr << "Error opening file" << endl; // Print an error message if the file could not be opened
     }
@@ -73,15 +81,15 @@ Athletes* scoreSort(Athletes* athletes) {
     for (int i = 0; i < numAthletes; i++)
     {
         if (i == numAthletes - 1)
+        {
             outputFile << athletes[i].name << "#" << athletes[i].score << "#" << athletes[i].school;
+        }
         else
+            cout << athletes[i].name << endl;
             outputFile << athletes[i].name << "#" << athletes[i].score << "#" << athletes[i].school << endl;
     }
 
 
     // Close the file
     outputFile.close();
-
-    // returns the structure array
-    return athletes;
 }
