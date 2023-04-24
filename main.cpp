f3ab5ab7413858abb946e5b921d3a59c8e42be6c
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-04-24 14:31:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,53 +3,36 @@
 #include <string>
 #include <fstream>
 #include <python.h>
+#include <windows.h>
 using namespace std;
 
-// function prototypes
-void getAthletes(vector<string>, string*, string*, double*);
-int displayMenu(int, bool);
-void scoreSort();
-vector<string> readFile(string);
-void throwTurns(string[], string*, int, int);
-void studLeaderboard(string*, string[], string[], int, int);
-void viewAthletes(string*, string*, double*, int);
-int getFlights(int);
-
-/*
-readFile accepts no arguments
-- reads the lines from a given file
-- outputs the vector of lines
-*/
-vector<string> readFile(string file_name)
+struct Athletes
 {
-	// declares variables
-	vector<string> lines;
-	string line;
+	string name;
+	string school;
+	string max;
 
-	// opens the file
-	ifstream in_file(file_name);
+};
 
-	// loops to read through each line in the file
-	// adds each line to the vector
-	int index = 0;
-	while (getline(in_file, line))
-	{
-		lines.push_back(line);
-		index++;
-	}
-
-	// closes the file
-	in_file.close();
-
-	return lines;
-}
+// function prototypes
+int displayMenu(int, bool);
+void scoreSort(Athletes*);
+void throwTurns(Athletes[], int, int);
+void studLeaderboard(string*, string[], string[], int, int);
+void viewAthletes(string*, string*, string*, int);
+int getFlights(int);
+void addAthlete();
+int getNumAthletes();
 
 int main()
 {
+	// repository creator
+	wstring filePath = L"C://track_data";
+	LPCWSTR path = filePath.c_str();
 
 	// python
 	const char* argv[2];
-	argv[0] = "C:\\Users\\SSC00042\\Desktop\\webscrape.py";
+	argv[0] = "C:\\track_data\\webscrape.py";
 
 	Py_Initialize();
 	PyObject* obj = Py_BuildValue("s", argv[0]);
@@ -68,36 +51,16 @@ int main()
 	bool throw_done = 0;
 	string file_name;
 
-	// gets the lines from the file from readFile
-	file_name = "C:\\Users\\SSC00042\\Desktop\\raw-athlete-database.txt";
-	lines = readFile(file_name);
-
-	// creates three pointer arrays for name, school, and max
-	// based on the size of the student vector
-	string* name = nullptr;
-	name = new string[lines.size()];
-	string* school = nullptr;
-	school = new string[lines.size()];
-	double* max = nullptr;
-	max = new double[lines.size()];
-
-	// calls getAthletes to input all of the athletes
-	// into a tri-parallel array
-	getAthletes(lines, name, school, max);
+	num_athletes = getNumAthletes();
+	Athletes* athletes = nullptr;
+	athletes = new Athletes[num_athletes];
 
 	// calls scoreSort to sort the athletes in the file
-	scoreSort();
-
-	// calls getAthletes again to get the sorted athletes
-	// and put them into a tri-parallel array
-	file_name = "C:\\Users\\SSC00042\\Desktop\\sorted-athlete-database.txt";
-	lines = readFile(file_name);
-	num_athletes = lines.size();
+	scoreSort(athletes);
+	// cout << athletes[0].name << endl;
 
 	// calls getFlights to get the number of flights from the user
 	flights = getFlights(num_athletes);
-	throwsArray = new string[num_athletes * rounds * flights];
-	getAthletes(lines, name, school, max);
 
 	int choice = 0;
 
@@ -106,37 +69,33 @@ int main()
 		choice = displayMenu(choice, throw_done);
 		cout << endl << endl;
 
-		// throw option
+		// add athlete option
 		if (choice == 1)
 		{
-			throwTurns(name, throwsArray, flights, num_athletes);
-			throw_done = 1;
+			// adds athlete
+			addAthlete();
+			scoreSort(athletes);
+			num_athletes = getNumAthletes();
+
 
 		}
-		/* view leaderboard option
+		// throw option
 		else if (choice == 2)
 		{
-			if (throw_done == 0)
-				"\nNo student has thrown.\n";
-			else
-				studLeaderboard(throwsArray, name, school, flights, num_athletes);
+			throwsArray = new string[num_athletes * 3 * flights];
+
+			for (int i =0; i < num_athletes; i++)
+			// runs the throws
+			throwTurns(athletes, flights, num_athletes);
 		}
-		*/
 
 
 
 
 	} while (choice != 6);
 
-	// deletes the pointer arrays
-	delete[] name;
-	name = nullptr;
-	delete[] school;
-	school = nullptr;
-	delete[] max;
-	max = nullptr;
+	// clears memory
+	delete[] throwsArray;
+	delete[] athletes;
 	return 0;
-
-	// string* ThrowsAway = nullptr;
-	// ThrowsAway = new string[athletes * num_athletes * flights]
 }
