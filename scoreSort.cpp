469c216626ae4e4a4ef4d1d9d1cde8387663d6e9
RikoSkra <121973962+RikoSkra@users.noreply.github.com>
2023-04-27 13:18:37 -0500

    Update scoreSort.cpp

--- scoreSort.cpp
+++ scoreSort.cpp
@@ -1,172 +1,145 @@
 #include <iostream>
-#include <fstream>
 #include <string>
-#include <set>
+#include <fstream>
+#include <sstream>
+//Cloven Lo
 
 using namespace std;
 
-const int MAX_ATHLETES = 100; // Maximum number of athletes that can be read from the input file
-
-struct Athlete {
+struct Athletes {
     string name;
     string score;
     string school;
-    double feet;
-    double inches;
+    double feet = 0.0;
+    double inches = 0.0;
 };
 
-// declares functions
-void swapAthletes(Athlete&, Athlete&);
-void sortAthletes(Athlete[], int);
-bool deleteDuplicates(Athlete[], int&);
-void writeAthletes(Athlete[], int);
+// declares function Prototypes
+// Changed to void since arrays are passed to functions by reference
+void output(Athletes [], int);
+void scoreSort(Athletes [], int&);
 
-// Swaps the values of two Athlete variables
-void swapAthletes(Athlete& a, Athlete& b) {
-    Athlete temp = a;
-    a = b;
-    b = temp;
+int main()
+{
+    // dynamically create an array of Throwers
+    Athletes* athleteArr = new Athletes[100];
+
+    int numAthletes = 0; // keep track of the number of Athletes objects in the array
+
+    // Populate and output the array of structs
+    cout << "Populating the structure from a file...\n\n";
+    scoreSort(athleteArr, numAthletes);
+    cout << "\nOutputting Athletes from the structure...\n\n";
+    output(athleteArr, numAthletes);
+
+    // delete the dynamic array
+    delete[] athleteArr;
 }
 
-// Sorts the array of Athletes in ascending order of score using bubble sort
-void sortAthletes(Athlete athletes[], int numAthletes) {
-    for (int i = 0; i < numAthletes - 1; i++) {
-        for (int j = 0; j < numAthletes - i - 1; j++) {
-            if (athletes[j].score > athletes[j + 1].score && athletes[j].score != "-1") {
-                swapAthletes(athletes[j], athletes[j + 1]);
-            }
-        }
-    }
-}
-
-// Deletes athletes with duplicate names from the array and returns true if any duplicates were found
-bool deleteDuplicates(Athlete athletes[], int& numAthletes) {
-    bool deleted = false;
-    for (int i = 0; i < numAthletes; i++) {
-        if ((i > 0 && athletes[i].name == athletes[i - 1].name) ||
-            (i < numAthletes - 1 && athletes[i].name == athletes[i + 1].name)) {
-            athletes[i].score = "-1";
-            deleted = true;
-        }
-    }
-    return deleted;
-}
-
-// Writes the sorted array of Athletes to an output file, deleting duplicates
-void writeAthletes(Athletes* athletes, int numAthletes) {
+void scoreSort(Athletes athletes[], int& numAthletes)
+{
+    // Declare Variables
+    string name, school, score;
+    double feet, inches;
 
     // Open the input file
-    ifstream inputFile("C:\\track_data\\raw-athletes.txt");
-    if (!inputFile) {
-        cerr << "Error opening file" << endl;
-        return;
-    }
-
-    // Open the output file
-    ofstream outputFile("C:\\track_data\\sorted-athletes.txt");
-    if (!outputFile) {
-        cerr << "Error opening file" << endl;
-        inputFile.close();
-        return;
-    }
-
-    // Declare an array to keep track of which Athletes to delete
-    bool deleteAthlete[MAX_ATHLETES] = { false };
-
-    // Read the input file line by line and mark duplicates for deletion
-    string line;
-    while (getline(inputFile, line)) {
-        try {
-            size_t pos1 = line.find("#");
-            string name = line.substr(0, pos1);
-
-            // Check if the name is a duplicate
-            bool duplicateFound = false;
-            for (int i = 0; i < numAthletes; i++) {
-                if (athletes[i].name == name) {
-                    if (!duplicateFound) {
-                        // This is the first occurrence of a duplicate name, don't delete it
-                        duplicateFound = true;
-                    }
-                    else {
-                        // This is the second occurrence of a duplicate name, mark it for deletion
-                        deleteAthlete[i] = true;
-                    }
-                }
-            }
-        }
-        catch (exception) {
-            // Handle any exceptions that may occur while processing the file
-            cerr << "Error processing line: " << line << endl;
-        }
-    }
-
-    // Write the sorted and de-duplicated Athletes to the output file
-    for (int i = 0; i < numAthletes; i++) {
-        if (!deleteAthlete[i]) {
-            outputFile << athletes[i].name << "#" << athletes[i].score << "#" << athletes[i].school << endl;
-        }
-    }
-
-    // Close the input and output files
-    inputFile.close();
-    outputFile.close();
-}
-
-void scoreSort(Athletes* athletes) {
-
-    ifstream inputFile("C:\\track_data\\raw-athlete-database.txt"); // Open the input file
+    ifstream inputFile("C:\\\\track_data\\\\raw-athlete-database.txt");
     if (!inputFile) { // Check if the input file was opened successfully
         cerr << "Error opening file" << endl; // Print an error message if the file could not be opened
+        return;
     }
 
-    int numAthletes = 0; // Counter to keep track of the number of Athlete objects read from the file
+    while (numAthletes < 100 && inputFile) { // Read Athlete objects from the file until the end of file is reached or 100 athletes have been read
 
-    string line;
-    while (getline(inputFile, line)) { // Read Athlete objects from the file until the end of file is reached
+        string line;
+        getline(inputFile, line);
+        if (line.empty()) {
+            continue; // Skip empty lines
+        }
 
-        try {
-            size_t pos1 = line.find("#"); // Find the position of the first hash symbol
-            size_t pos2 = line.find_last_of("#"); // Find the position of the last hash symbol
-            size_t pos3 = line.find_last_of('-');
-            athletes[numAthletes].name = line.substr(0, pos1); // Extract the name from the line
-            athletes[numAthletes].score = line.substr(pos1 + 1, pos2 - pos1 - 1); // Extract the score from the line
-            athletes[numAthletes].school = line.substr(pos2 + 1); // Extract the school from the line
-            if (athletes[numAthletes].score != "-1")
-            {
-                athletes[numAthletes].feet = stod(line.substr(pos1 + 1, pos3 - pos1 - 1));
-                athletes[numAthletes].inches = stod(line.substr(pos3 + 1, pos2 - pos3 - 1));
-            }
-            numAthletes++;
+        size_t pos1 = line.find("#"); // Find the position of the first hash symbol
+        size_t pos2 = line.find_last_of("#"); // Find the position of the last hash symbol
+        size_t pos3 = line.find_last_of('-');
+
+        // Check if pos1 and pos3 are not equal to string::npos before extracting the feet and inches values #PROBLEM HERE!!!
+        if (pos1 == string::npos || pos3 == string::npos) {
+            cerr << "Error: Invalid input format in input line " << numAthletes + 1 << endl;
+            continue; // Skip the current line and move on to the next line
         }
-        catch (exception e) // Catch the exception thrown by stod() function
-        {
-            cerr << "Error converting score for athlete " << numAthletes + 1 << endl;
-            cerr << e.what() << endl;
+
+        // Extract each substring from the input line and assign to the appropriate variable
+        name = line.substr(0, pos1);
+        score = line.substr(pos1 + 1, pos2 - pos1 - 1);
+        school = line.substr(pos2 + 1);
+
+        // Check if the feet and inches substrings can be converted to valid double values
+        // using stringstream
+        stringstream ss(line.substr(pos1 + 1, pos3 - 1));
+        if (!(ss >> feet)) {
+            cerr << "" << numAthletes + 1 << endl;
+            continue; // Skip the current line and move on to the next line
         }
+
+        ss = stringstream(line.substr(pos3 + 1, pos2 - 1));
+        if (!(ss >> inches)) {
+            cerr << "Error: Invalid inches value in input line " << numAthletes + 1 << endl;
+            continue; // Skip the current line and move on to the next line
+        }
+
+        // Add each to the structure
+        athletes[numAthletes].name = name;
+        athletes[numAthletes].score = score;
+        athletes[numAthletes].feet = feet;
+        athletes[numAthletes].inches = inches;
+        athletes[numAthletes].school = school;
+        numAthletes++; // Increment the counter for the number of Athlete objects read
     }
-
-    sortAthletes(athletes, numAthletes); // Sort the array of Athletes in ascending order of score using bubble sort
     inputFile.close();
+    cout << "\nThere were " << numAthletes << " total lines read from the file.\n";
+    cout << "\n------------------------------";
 
-    ofstream outputFile("C:\\track_data\\sorted-athletes.txt"); // Open the output file
-    if (!outputFile) { // Check if the output file was opened successfully
-        cerr << "Error opening file" << endl; // Print an error message if the file could not be opened
+    // Sort the athletes array by feet in ascending order using selection sort algorithm
+    for (int i = 0; i < numAthletes - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < numAthletes; j++) {
+            if (athletes[j].feet < athletes[minIndex].feet) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            Athletes temp = athletes[i];
+            athletes[i] = athletes[minIndex];
+            athletes[minIndex] = temp;
+        }
+    }
+}
+
+
+void output(Athletes arr[], int size)
+{
+    // Remove duplicates by comparing each athlete's name to the names of the other athletes
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i].name == arr[j].name) {
+                // Remove the duplicate athlete object from the array
+                for (int k = j; k < size - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                size--; // Decrease the size of the array
+                j--; // Decrement j so it continues to check the current position again
+            }
+        }
     }
 
-    for (int i = 0; i < numAthletes; i++)
+    int index = 0;
+    for (index = 0; index < size; index++)
     {
-        if (i == numAthletes - 1)
-        {
-            outputFile << athletes[i].name << "#" << athletes[i].score << "#" << athletes[i].school;
-        }
-        else {
-            cout << athletes[i].name << endl;
-            outputFile << athletes[i].name << "#" << athletes[i].score << "#" << athletes[i].school << endl;
-        }
+        //Output the structure
+        cout << "Thrower: " << arr[index].name << endl;
+        cout << "School: " << arr[index].school << endl;
+        cout << "Distance Thrown: " << arr[index].feet <<
+             " feet, " << arr[index].inches << " inches\n";
+        cout << endl;
     }
-
-
-    // Close the file
-    outputFile.close();
+    cout << "There were " << index << " total throwers in the structure.\n";
 }
