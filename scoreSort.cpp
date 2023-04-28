7d0f40884c8283fcfe82707b76d7149a2f2c312f
RikoSkra <121973962+RikoSkra@users.noreply.github.com>
2023-04-28 13:42:41 -0500

    Update scoreSort.cpp

--- scoreSort.cpp
+++ scoreSort.cpp
@@ -14,68 +14,88 @@ struct Athletes {
     double inches = 0.0;
 };
 
-// declares function Prototypes
-// Changed to void since arrays are passed to functions by reference
+// Uses getNumAthletes
+void getNumAthletes(int numAthletes);
+void populateStructure(int);
 void output(Athletes [], int);
-void scoreSort(Athletes [], int&);
+void scoreSort(Athletes [], double);
 
-void scoreSort(Athletes athletes[], int& numAthletes)
+// Uses numAthlete from getNumAthletes.cpp
+
+
+void populateStructure(int numAthletes)
 {
+    Athletes* athleteArr = new Athletes[numAthletes];
+
+    // Populate and output the array of structs
+    cout << "Populating the structure from a file...\n\n";
+    scoreSort(athleteArr, numAthletes);
+    cout << "\nOutputting Athletes from the structure...\n\n";
+    output(athleteArr, numAthletes);
+
+    // delete the dynamic array
+    delete[] athleteArr;
+}
+
+void scoreSort(Athletes athletes[], double numAthletes)
+{
+
     // Declare Variables
-    string name, school, score;
-    double feet, inches;
+    string name, school, score, line;
+    double feet, inches, numAthlete;
 
     // Open the input file
-    ifstream inputFile("C:\\\\track_data\\\\raw-athlete-database.txt");
+    ifstream inputFile("C:\\track_data\\raw-athlete-database.txt");
     if (!inputFile) { // Check if the input file was opened successfully
         cerr << "Error opening file" << endl; // Print an error message if the file could not be opened
         return;
     }
 
-    while (numAthletes < 100 && inputFile) { // Read Athlete objects from the file until the end of file is reached or 100 athletes have been read
+    while( getline(inputFile, line)) {
+        numAthlete++;
+        while (numAthlete < numAthletes && inputFile) {
+            getline(inputFile, line);
+            if (line.empty()) {
+                continue; // Skip empty lines
+            }
 
-        string line;
-        getline(inputFile, line);
-        if (line.empty()) {
-            continue; // Skip empty lines
+            size_t pos1 = line.find("#"); // Find the position of the first hash symbol
+            size_t pos2 = line.find_last_of("#"); // Find the position of the last hash symbol
+            size_t pos3 = line.find_last_of('-');
+
+            // Check if pos1 and pos3 are not equal to string::npos before extracting the feet and inches values #PROBLEM HERE!!!
+            if (pos1 == string::npos || pos3 == string::npos) {
+                cerr << "Error: Invalid input format in input line " << numAthletes + 1 << endl;
+                continue; // Skip the current line and move on to the next line
+            }
+
+            // Extract each substring from the input line and assign to the appropriate variable
+            name = line.substr(0, pos1);
+            score = line.substr(pos1 + 1, pos2 - pos1 - 1);
+            school = line.substr(pos2 + 1);
+
+            // Check if the feet and inches substrings can be converted to valid double values
+            // using stringstream
+            stringstream ss(line.substr(pos1 + 1, pos3 - 1));
+            if (!(ss >> feet)) {
+                cerr << "" << numAthletes + 1 << endl;
+                continue; // Skip the current line and move on to the next line
+            }
+
+            ss = stringstream(line.substr(pos3 + 1, pos2 - 1));
+            if (!(ss >> inches)) {
+                cerr << "Error: Invalid inches value in input line " << numAthletes + 1 << endl;
+                continue; // Skip the current line and move on to the next line
+            }
+
+            // Add each to the structure
+            athletes[numAthletes].name = name;
+            athletes[numAthletes].score = score;
+            athletes[numAthletes].feet = feet;
+            athletes[numAthletes].inches = inches;
+            athletes[numAthletes].school = school;
+            numAthletes++; // Increment the counter for the number of Athlete objects read
         }
-
-        size_t pos1 = line.find("#"); // Find the position of the first hash symbol
-        size_t pos2 = line.find_last_of("#"); // Find the position of the last hash symbol
-        size_t pos3 = line.find_last_of('-');
-
-        // Check if pos1 and pos3 are not equal to string::npos before extracting the feet and inches values #PROBLEM HERE!!!
-        if (pos1 == string::npos || pos3 == string::npos) {
-            cerr << "Error: Invalid input format in input line " << numAthletes + 1 << endl;
-            continue; // Skip the current line and move on to the next line
-        }
-
-        // Extract each substring from the input line and assign to the appropriate variable
-        name = line.substr(0, pos1);
-        score = line.substr(pos1 + 1, pos2 - pos1 - 1);
-        school = line.substr(pos2 + 1);
-
-        // Check if the feet and inches substrings can be converted to valid double values
-        // using stringstream
-        stringstream ss(line.substr(pos1 + 1, pos3 - 1));
-        if (!(ss >> feet)) {
-            cerr << "" << numAthletes + 1 << endl;
-            continue; // Skip the current line and move on to the next line
-        }
-
-        ss = stringstream(line.substr(pos3 + 1, pos2 - 1));
-        if (!(ss >> inches)) {
-            cerr << "Error: Invalid inches value in input line " << numAthletes + 1 << endl;
-            continue; // Skip the current line and move on to the next line
-        }
-
-        // Add each to the structure
-        athletes[numAthletes].name = name;
-        athletes[numAthletes].score = score;
-        athletes[numAthletes].feet = feet;
-        athletes[numAthletes].inches = inches;
-        athletes[numAthletes].school = school;
-        numAthletes++; // Increment the counter for the number of Athlete objects read
     }
     inputFile.close();
     cout << "\nThere were " << numAthletes << " total lines read from the file.\n";
