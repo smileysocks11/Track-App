09716bdd31007ceec89c59edecae00c9f752f40d
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-05-02 14:35:43 -0500

    Update getFlights.cpp

--- getFlights.cpp
+++ getFlights.cpp
@@ -1,18 +1,14 @@
-/*
- * brighton hilyard's file
-*/
-
 #include <iostream>
 #include <string>
 using namespace std;
 
-int getFlights(int num_athletes) {
+int getFlights(int& num_athletes) {
     /*
      * gets the amount of flights for the meet
      * returns to main
      */
-    int flights;
-    cout << "There are " << num_athletes << " athletes attending the meet.\n";
+    int flights = 0;
+    cout << "\nThere are " << num_athletes << " athletes attending the meet.\n";
     cout << "How any flights would you like in this meet? ";
     cin >> flights;
 
