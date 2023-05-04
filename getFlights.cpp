ff050ce94e91613badf986ecbee89db8921c4e75
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-05-04 14:49:50 -0500

    Update getFlights.cpp

--- getFlights.cpp
+++ getFlights.cpp
@@ -7,17 +7,15 @@
 #include "Athletes.h"
 using namespace std;
 
-
-
-int getFlights(Athletes a[], int num_athletes) {
-    /*
-     * gets the amount of flights for the meet
-     * returns to main
-     */
-    int flights = 0;
-    cout << "\nThere are " << num_athletes << " athletes attending the meet.\n";
-    cout << "How any flights would you like in this meet? ";
-    cin >> flights;
+int getFlights(Athletes a[], int& num_athletes) {
+	/*
+	 * gets the amount of flights for the meet
+	 * returns to main
+	 */
+	int flights = 0;
+	cout << "\nThere are " << num_athletes << " athletes attending the meet.\n";
+	cout << "How any flights would you like in this meet? ";
+	cin >> flights;
 
 	int count = 0;
 	int div = num_athletes / flights;
