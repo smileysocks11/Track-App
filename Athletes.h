0f07ed1fd2b9a5a3d6ac83d5430101614c0171c3
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-05-04 14:14:27 -0500

    Create Athletes.h

---
+++ Athletes.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef ATHLETES_H
+#include <string>
+using namespace std;
+
+struct Athletes {
+    string name;
+    string score;
+    string school;
+    double feet;
+    double inches;
+    string max;
+    string throw1, throw2, throw3;
+    string bestThrow;
+    int flightNum = 0;
+};
+
+#endif
