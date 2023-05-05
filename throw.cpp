8cc2df1e12ac2df5544a0af6be355cc119f46b35
Landon-Marcotte <marcottelandon9@gmail.com>
2023-05-05 11:49:49 -0700

    update throw

--- throw.cpp
+++ throw.cpp
@@ -13,8 +13,8 @@ void throwTurns(Athletes a[], int flights, int athletes);
 
 void throwTurns(Athletes a[], int flights, int athletes)
 {
-	/*use the loop to loop thorugh the array until all the athles from a single flight has
-	the array is a 3D array where the first colum has the names of the students
+	/*use the loop to loop through the array until all the athletes from a single flight has
+	thrown and moves on to the next athletes in the structure bassed off their throw
 	the second has the school they go to and the third has their best throw
 	A failed attempt is a DQ in the stystem
 	fill the first flight with more people than tht others if there are left*/
