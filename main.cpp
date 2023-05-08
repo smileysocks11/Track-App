6ec56cefe77f25d1269fb79336d61e2f2966aeeb
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-05-08 13:33:38 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,15 +30,15 @@ int main()
 	LPCWSTR path = filePath.c_str();
 
 	// python
-	// const char* argv[2];
-	// argv[0] = "C:\\track_data\\webscrape.py";
+	const char* argv[2];
+	argv[0] = "C:\\track_data\\webscrape.py";
 
-	/*Py_Initialize();
+	Py_Initialize();
 	PyObject* obj = Py_BuildValue("s", argv[0]);
 	FILE* file = _Py_fopen_obj(obj, "r");
 	if (file)
 		PyRun_SimpleFile(file, argv[0]);
-	Py_Finalize(); */
+	Py_Finalize();
 
 
 	// declares variables
