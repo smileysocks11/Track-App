f0ba17e9918142115f8ef74ba3d3981ec11c9fb8
BananaGuard16 <90475977+BananaGuard16@users.noreply.github.com>
2023-05-09 14:43:11 -0500

    Update athleteChecker.cpp
    Add throw4 and change qualifications for a throw to be missing (missing throw is -1, not empty)

--- athleteChecker.cpp
+++ athleteChecker.cpp
@@ -10,7 +10,7 @@
  * and calls athleteChecker to check each athlete's struct
 */
 
-bool athleteChecker(const Athletes& athlete) {
+bool athleteChecker(const Athletes& athlete, int rounds) {
     /*
      * checks if the provided athlete has missed any throws
      * returns a bool finishedThrowing
@@ -21,17 +21,19 @@ bool athleteChecker(const Athletes& athlete) {
     int missedThrows = 0;
     bool finishedThrowing = true;
 
-    if (athlete.throw1.empty()) missedThrows += 1;
+    if (athlete.throw1 == "-1") missedThrows += 1;
 
-    if (athlete.throw2.empty()) missedThrows += 1;
+    if (athlete.throw2 == "-1") missedThrows += 1;
 
-    if (athlete.throw3.empty()) missedThrows += 1;
+    if (athlete.throw3 == "-1") missedThrows += 1;
+
+    if (athlete.throw4 == "-1") missedThrows += 1;
 
     if (missedThrows > 0) {
         finishedThrowing = false;
 
         std::cout << athlete.name << " has missed " << missedThrows << " throw.\n";
-        
+
     }
 
     return finishedThrowing;
