50822213fbf096def4b984890594a68c4cc191ba
Sophie <90475851+smileysocks11@users.noreply.github.com>
2023-05-09 13:34:08 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 // function prototypes
 int displayMenu(int, bool);
 void scoreSort(Athletes[], int&);
-void throwTurns(Athletes[], int, int);
+void throwTurns(Athletes[], int, int, int);
 void finals(Athletes[], int);
 void studLeaderboard(Athletes[], int&);
 void viewAthletes(Athletes[], int, int, bool);
@@ -19,9 +19,11 @@ void getFlights(Athletes[], int, string, int);
 void addAthlete();
 int getNumAthletes();
 void manualAdjust(Athletes[], int&, int&);
-void findBestAttempt(Athletes[], int&);
+void findBestAttempt(Athletes[], int&, int);
 void finals(Athletes[], int);
 bool athleteChecker(const Athletes& athlete);
+int getFlightInput(int);
+int getThrowInput();
 
 int main()
 {
@@ -44,11 +46,9 @@ int main()
 	// declares variables
 	vector<string> lines;
 	int num_athletes;
-	int flights;
-	const int rounds = 3;
+	int flights, throws;
 	bool throw_done = 0;
 	string file_name, input;
-	bool flightGood = false, retry = false;
 
 	num_athletes = getNumAthletes();
 	Athletes* athletes = nullptr;
@@ -56,60 +56,9 @@ int main()
 
 	// calls scoreSort to sort the athletes in the file
 	scoreSort(athletes, num_athletes);
-
-	// calls getFlights to get the number of flights from the user
-	// Loop until flights is good
-	do
-	{
-		cout << "\nThere are " << num_athletes << " athletes attending the meet.\n";
-		cout << "How any flights would you like in this meet? ";
-		cin >> input;
-
-		// Validate user input
-		try
-		{
-			flights = stoi(input);
-
-			// Make sure flights is greater than 0
-			if (flights <= 0)
-				throw invalid_argument("\nError: Enter a value greater than 0\n");
-
-			// Loop until the user enters y or n
-			do
-			{
-				// Make sure the number is good
-				cout << "With " << flights << " flights, there will be about " << num_athletes / flights
-					<< " athletes per flight. Is this number appropriate? (y/n)\n> ";
-				cin >> input;
-
-				// Validate user input
-				if (input == "y" || input == "Y")
-				{
-					flightGood = true;
-					retry = true;
-				}
-				else if (input == "n" || input == "N")
-				{
-					flightGood = false;
-					cout << "Reenter number for flights:\n";
-					retry = true;
-				}
-				else
-				{
-					cout << "\nError: Please enter y or n\n\n";
-					retry = false;
-				}
-			} while (!retry);
-
-		}
-		catch (exception)
-		{
-			cout << "\nError: Enter a numerical value greater than 0 for flights\n";
-			flightGood = false;
-		}
-
-	} while (!flightGood);
+	flights = getFlightInput(num_athletes);
 	getFlights(athletes, num_athletes, input, flights);
+	throws = getThrowInput();
 
 	int choice = 0;
 
@@ -139,10 +88,10 @@ int main()
 		// throw option
 		else if (choice == THROW)
 		{
-			throwTurns(athletes, flights, num_athletes);
+			throwTurns(athletes, flights, num_athletes, throws);
 			throw_done = 1;
 			ADD = 10, THROW = 10, VIEW = 1, ADJUST = 10, QUIT = 5, LB = 2, FINALS = 3, CHECK = 4;
-			findBestAttempt(athletes, num_athletes);
+			findBestAttempt(athletes, num_athletes, throws);
 
 		}
 		//view althletes
